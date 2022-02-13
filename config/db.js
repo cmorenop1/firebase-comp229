722de2ae7b5e802a48f66ef78f804d4d6dbdba22
JViniX <47263194+JViniX@users.noreply.github.com>
2022-02-13 18:19:19 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // Do not use it in production
-let DB_CONNECTION = "mongodb+srv://dbadmin:GriLAkPfVZebeg58@clusters002.5ikxx.mongodb.net/comp229002"
+let DB_CONNECTION = "mongodb+srv://dbadmin:GriLAkPfVZebeg58@clusters002.5ikxx.mongodb.net/comp229002?retryWrites=true&w=majority"
 
 // Database Setup
 let mongoose = require('mongoose');
